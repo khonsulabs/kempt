ec45a2ffcf5a0d9b722ebc424f48c3302d6a0a56
kl-botsu <botsu@khonsulabs.com>
2023-05-29 22:48:54 +0000

    Deploying to gh-pages from @ khonsulabs/objectmap@0cef67f5aea8aca20d300d5f7db94f0384c29956 🚀

--- main/objectmap/sidebar-items.js
+++ main/objectmap/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Entry"],"struct":["Drain","Field","IntoIter","IntoValues","Iter","IterMut","ObjectMap","OccupiedEntry","VacantEntry","Values"]};
+window.SIDEBAR_ITEMS = {"enum":["Entry","SearchKey"],"struct":["Drain","Field","IntoIter","IntoValues","Iter","IterMut","ObjectMap","OccupiedEntry","VacantEntry","Values"],"trait":["Sort"]};
